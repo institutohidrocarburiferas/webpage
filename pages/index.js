500d220c3851e9e0d3c2280834254a9871effd31
andresguanov <andresguanov@gmail.com>
2022-09-11 19:22:26 -0500

    [Add] Tailwind config

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Instituto de Investigaciones Hidrocarburíferas</title>
+        <meta name="description" content="Instituto de Investigaciones Hidrocarburíferas" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    </div>
+  )
+}
