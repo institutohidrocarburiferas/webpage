748907428bd18a41f2f23aa22d729544b088d43f
andresguanov <andresguanov@gmail.com>
2022-09-16 23:12:16 -0500

    [Fix] styles index page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { BreakSection } from '@components/BreakSection'
 import { NoticeCard } from '@components/NoticeCard'
 import { TitlePage } from '@components/TitlePage'
 import Head from 'next/head'
@@ -13,16 +12,14 @@ export default function Home () {
       </Head>
 
       <TitlePage title={'Título'} image={'/prueba.png'} />
-
       <main className='w-screen dark:text-gray-100'>
         <article className="max-w-sm mx-auto my-10 sm:max-w-lg md:max-w-2xl" >
           <p className='leading-loose tracking-wide md:text-lg'>IIH es un organismo de investigación que desarrolla investigaciones hidrocarburíferas y de energía bajo la comprensión profunda de aspectos cruciales en la relación de los sistemas socioeconómicos y los ecosistemas; tales como: peak oil, limitaciones biofísicas, transición y descarbonización de las economías y sistemas de gobernanza, incluidas las formas de abordar nuestros patrones de producción y consumo. Finalmente contribuye al debate de sostenibilidad más allá de la ilusión de una disponibilidad energética ilimitada y una solución tecnológica.
           </p>
         </article>
-
-        <section className='relative'>
-          <BreakSection position={'-top-20'} />
-          <div className='flex flex-col items-center justify-center gap-10 pt-10 pb-40 pl-4 pr-8 mt-40 bg-gray-400 md:flex-row'>
+        <section className='relative flex flex-col gap-10'>
+          <h2 className='px-2 py-1 mx-auto text-3xl border-b-2 sm:px-20 md:px-40'>Noticias más importantes</h2>
+          <div className='flex flex-col items-center justify-center gap-10 pb-20 pl-4 pr-8 md:flex-row'>
 
             <NoticeCard
               title={'Poster SAMO'}
@@ -41,13 +38,7 @@ export default function Home () {
             />
           </div>
         </section>
-
       </main>
-      {/* <Footer
-        bgColor={'gray-400'}
-        image={'/prueba.png'}
-      /> */}
-
     </div>
   )
 }
