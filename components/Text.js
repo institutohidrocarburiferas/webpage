a9e136de50d993529b7351c1cfc439349151b41c
andresguanov <andresguanov@gmail.com>
2022-09-21 11:11:14 -0500

    [Modify] Text size

--- components/Text.js
+++ components/Text.js
@@ -1,5 +1,5 @@
 export function Text ({ children }) {
-  return <p className='mt-6 leading-loose tracking-wide text-justify md:text-lg'>
+  return <p className='mt-6 leading-loose tracking-wide text-justify md:text-md lg:text-xl'>
     {children}
   </p>
 }
