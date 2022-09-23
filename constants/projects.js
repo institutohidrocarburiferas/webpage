d84f160a550448ecf34c88a6c67b633e8165b907
andresguanov <andresguanov@gmail.com>
2022-09-23 12:30:22 -0500

    [Add] images to each publication

--- constants/projects.js
+++ constants/projects.js
@@ -3,22 +3,23 @@ const pjt = '/proyectos-investigacion'
 export const projects = [
   {
     title: 'Contratos petroleros',
-    image: '',
+    image: '/prueba.png',
+    // image: `${pjt}/contratos-petroleros/contratos-petroleros.jpeg`,
     url: `${pjt}/contratos-petroleros`
   },
   {
     title: 'Senior 2021',
-    image: '/externalLinks/revista-figempa-logo.png',
+    image: '/prueba.png',
     url: `${pjt}/senior-2021`
   },
   {
     title: 'Sistema de Monitoreo del Bioindicadores ANZU',
-    image: '',
+    image: '/prueba.png',
     url: `${pjt}/sistema-de-monitoreo-de-bioindicadores-ANZU`
   },
   {
     title: 'Servicios Ambientales',
-    image: '',
+    image: '/prueba.png',
     url: `${pjt}/servicios-ambientales`
   },
 ]
--- constants/publications.js
+++ constants/publications.js
@@ -1,10 +1,17 @@
 export const publications = [
   {
     title: 'Construcción de Escenarios Energéticos para la Extracción de petróleo en el Bloque 43 de la Amazonía Ecuatoriana (pág 58 -71) ',
-    url: 'https://enerlac.olade.org/index.php/ENERLAC/issue/view/11'
+    image: '/publicaciones/escenarios-energeticos.png',
+    url: 'https://enerlac.olade.org/index.php/ENERLAC/article/view/165/251'
   },
   {
     title: 'Exploration of the environmental implications of ageing conventional oil reserves with relational analysis',
+    image: '/publicaciones/exploration-environmental-implications.png',
     url: 'https://www.sciencedirect.com/science/article/pii/S0048969720359003'
+  },
+  {
+    title: 'Understanding The Future Metabolism of Ecuador’s Energy System Using MuSIASEM (pág 109 - 110)',
+    image: '/publicaciones/future-metabolism-ecuador.png',
+    url: 'https://samo2022.math.fsu.edu/sites/g/files/upcbnu3196/files/2022-03/booklet.pdf#page=111'
   }
 ]
