8d56f2ace10aea3cae3f505a35918e9343e55b75
andresguanov <andresguanov@gmail.com>
2022-10-13 19:42:46 -0500

    [UPDATE] footer size

--- components/Footer.js
+++ components/Footer.js
@@ -9,34 +9,22 @@ export function Footer ({ bgColor, image = '/prueba.png' }) {
     backgroundRepeat: 'repeat',
     // backgroundSize: 'cover',
   }
-  return <footer className="relative bg-black/80">
+  return <footer className="relative">
 
     <svg className="absolute bg-transparent" xmlns="http://www.w3.org/2000/svg" width="100%" height="80px" viewBox="0 0 1280 140" preserveAspectRatio="none">
-      <g className={`${bgColor}`}><path d="M978.81 122.25L0 0h1280l-262.1 116.26a73.29 73.29 0 0 1-39.09 5.99z" fillOpacity=".5" /><path d="M983.19 95.23L0 0h1280l-266 91.52a72.58 72.58 0 0 1-30.81 3.71z" /></g></svg>
+      <g className={`${bgColor}`}><path d="M978.81 122.25L0 0h1280l-262.1 116.26a73.29 73.29 0 0 1-39.09 5.99z" fillOpacity=".5" /><path d="M983.19 95.23L0 0h1280l-266 91.52a72.58 72.58 0 0 1-30.81 3.71z" /></g>
+    </svg>
     <section
-      style={bgStyle}
-      className="flex flex-col items-center h-auto gap-5 px-20 pt-32 pb-20 font-bold text-white md:justify-around md:flex-row"
+    style={bgStyle}
+    className='absolute w-full h-full -z-10'
+    >
 
+    </section>
+    <section
+      className="flex flex-col items-center h-auto gap-5 px-12 pt-32 pb-5 font-bold text-white md:pt-12 md:justify-around md:flex-row bg-black/60"
     >
       <section className='text-3xl md:text-xl lg:text-3xl md:w-1/4'>
-        <span>Instituto de Investigaciones Hidrocarburíferas</span>
-      </section>
-      <section className='flex flex-col gap-3 md:w-2/4 '>
-        <span>Universidad Central del Ecuador</span>
-        <span>Ingreso por las Calles Jerónimo Leyton y Gilberto Gatto Sobral </span>
-        <span>Facultad de Ingeniería en Geología, Petróleos, Minas y Ambiental - FIGEMPA </span>
-        <span>Segundo piso, Área Posgrado </span>
-        <span>Quito - Ecuador </span>
-      </section>
-
-      <section className='flex flex-col items-center justify-center gap-5 md:w-1/4 place-content-center'>
-        <Image
-          width={150}
-          height={150}
-          src="/UCE-logo.png"
-          alt="Universidad Central del Ecuador Logo"
-        />
-        <Image
+      <Image
           width={341}
           height={100}
           className="bg-white rounded-lg"
@@ -44,7 +32,21 @@ export function Footer ({ bgColor, image = '/prueba.png' }) {
           alt="Instituto de Investigaciones Logo"
         />
       </section>
+      <section className='flex flex-col gap-3 md:w-2/5 '>
+        <span>Universidad Central del Ecuador</span>
+        <span>Ingreso por las Calles Jerónimo Leyton y Gilberto Gatto Sobral </span>
+        <span>Facultad de Ingeniería en Geología, Petróleos, Minas y Ambiental - FIGEMPA </span>
+        <span>Segundo piso, Área Posgrado </span>
+        <span>Quito - Ecuador </span>
+      </section>
+      <section className='flex flex-col items-center justify-center gap-5 md:w-1/4 place-content-center'>
+        <Image
+          width={150}
+          height={150}
+          src="/UCE-logo.png"
+          alt="Universidad Central del Ecuador Logo"
+        />
+      </section>
     </section>
-
   </footer>
 }
