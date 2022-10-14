3af2bf5aa05eda93019709accd143eefc7a8ff46
andresguanov <andresguanov@gmail.com>
2022-10-14 14:06:56 -0500

    [FIX] text spaces

--- components/Footer.js
+++ components/Footer.js
@@ -2,29 +2,32 @@ import Image from 'next/image'
 
 export function Footer ({ bgColor, image = '/prueba.png' }) {
   bgColor = bgColor ? `fill-${bgColor}` : 'fill-white dark:fill-gray-900'
-
   const bgStyle = {
     backgroundImage: `url(${image})`,
     backgroundPosition: 'right bottom',
     backgroundRepeat: 'repeat',
     // backgroundSize: 'cover',
   }
-  return <footer className="relative">
 
+  return <footer className="relative">
+    {/* SVG top image */}
     <svg className="absolute bg-transparent" xmlns="http://www.w3.org/2000/svg" width="100%" height="80px" viewBox="0 0 1280 140" preserveAspectRatio="none">
       <g className={`${bgColor}`}><path d="M978.81 122.25L0 0h1280l-262.1 116.26a73.29 73.29 0 0 1-39.09 5.99z" fillOpacity=".5" /><path d="M983.19 95.23L0 0h1280l-266 91.52a72.58 72.58 0 0 1-30.81 3.71z" /></g>
     </svg>
-    <section
-    style={bgStyle}
-    className='absolute w-full h-full -z-10'
-    >
 
-    </section>
-    <section
-      className="flex flex-col items-center h-auto gap-5 px-12 pt-32 pb-5 font-bold text-white md:pt-12 md:justify-around md:flex-row bg-black/60"
+    {/* Background image */}
+    <div
+      style={bgStyle}
+      className='absolute w-full h-full -z-10'
+    ></div>
+
+    {/* Footer container */}
+    <div
+      className="flex flex-col items-center h-auto gap-8 px-12 pt-32 pb-5 font-semibold text-white md:pt-12 md:flex-row bg-black/60"
     >
+      {/* Left section */}
       <section className='text-3xl md:text-xl lg:text-3xl md:w-1/4'>
-      <Image
+        <Image
           width={341}
           height={100}
           className="bg-white rounded-lg"
@@ -32,13 +35,17 @@ export function Footer ({ bgColor, image = '/prueba.png' }) {
           alt="Instituto de Investigaciones Logo"
         />
       </section>
-      <section className='flex flex-col gap-3 md:w-2/5 '>
+
+      {/* Middle section */}
+      <section className='flex flex-col gap-1 md:w-1/2 '>
         <span>Universidad Central del Ecuador</span>
         <span>Ingreso por las Calles Jerónimo Leyton y Gilberto Gatto Sobral </span>
         <span>Facultad de Ingeniería en Geología, Petróleos, Minas y Ambiental - FIGEMPA </span>
         <span>Segundo piso, Área Posgrado </span>
         <span>Quito - Ecuador </span>
       </section>
+
+      {/* Right section */}
       <section className='flex flex-col items-center justify-center gap-5 md:w-1/4 place-content-center'>
         <Image
           width={150}
@@ -47,6 +54,6 @@ export function Footer ({ bgColor, image = '/prueba.png' }) {
           alt="Universidad Central del Ecuador Logo"
         />
       </section>
-    </section>
+    </div>
   </footer>
 }
