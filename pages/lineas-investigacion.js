88df785e138eaa64ce7e794253945088a5716038
andresguanov <andresguanov@gmail.com>
2022-10-17 20:54:29 -0500

    [UPDATE] lineas de investigacion

--- pages/lineas-investigacion.js
+++ pages/lineas-investigacion.js
@@ -1,45 +1,64 @@
-import { TitlePage } from '@components/TitlePage'
-import Head from 'next/head'
+import { Content } from '@components/Content'
+
+const pageContent = {
+  title: 'Resumen',
+  description: 'Resumen IIH',
+  image: '/prueba.png',
+  content: [{
+    label: 'Hidrocarburos',
+    items: [
+      'Desarrollo biotecnológico aplicado a la industria',
+      'Simulación y modelamiento matemático ',
+      'Uso de tecnologías de la información y comunicación',
+      'Producción de petróleo pesado',
+    ]
+  },
+  {
+    label: 'Sostenibilidad',
+    items: [
+      'Gestión sostenible de recursos naturales',
+      'Economía y sociedad',
+      'Complejidad-energía-sostenibilidad',
+    ]
+  },
+  {
+    label: 'Energía',
+    items: [
+      'Relaciones economía, ambiente y sociedad',
+      'Relaciones economía, ambiente y sociedad',
+      'Transición energética',
+    ]
+  },
+  ]
+}
 
 export default function LineasInvestigacion () {
+  const sections = pageContent.content.map(({ label, items }) => (
+    <article
+      className="max-w-sm mx-auto my-10 sm:max-w-lg md:max-w-2xl"
+      key={label}
+    >
+      <h2 className='mt-10 mb-4 text-3xl font-bold border-b-2'>{label}</h2>
+      {items.map(item => (
+        <ul
+          className='pl-5 leading-loose tracking-wide list-disc md:text-lg style'
+          key={item}
+        >
+          <li className="my-2.5">{item}</li>
+        </ul>
+      ))}
+    </article>
+  ))
+
   return (
-    <div className="overflow-hidden ">
-      <Head>
-        <title>Resumen | IIH</title>
-        <meta name="description" content="Resumen IIH" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <TitlePage title={'Líneas de investigación'} image={'/prueba.png'} />
-
+    <Content
+      title={pageContent.title}
+      description={pageContent.description}
+      image={pageContent.image}
+    >
       <main className='w-screen dark:text-gray-100'>
-        <article className="max-w-sm mx-auto my-10 sm:max-w-lg md:max-w-2xl" >
-          <h2 className='mt-10 mb-4 text-3xl font-bold border-b-2'>Hidrocarburos</h2>
-          <ul className='pl-5 leading-loose tracking-wide list-disc md:text-lg style'>
-            <li className="my-2.5">Desarrollo biotecnológico aplicado a la industria </li>
-            <li className="my-2.5">Simulación y modelamiento matemático </li>
-            <li className="my-2.5">Uso de tecnologías de la información y comunicación </li>
-            <li className="my-2.5">Producción de petróleo pesado </li>
-          </ul>
-
-          <h2 className='mt-10 mb-4 text-3xl font-bold border-b-2'>Sostenibilidad</h2>
-          <ul className='pl-5 leading-loose tracking-wide list-disc md:text-lg'>
-            <li className="my-2.5">Gestión sostenible de recursos naturales </li>
-            <li className="my-2.5">Economía y sociedad </li>
-            <li className="my-2.5">Complejidad-energía-sostenibilidad  </li>
-          </ul>
-
-          <h2 className='mt-10 mb-4 text-3xl font-bold border-b-2'>Energía</h2>
-          <ul className='pl-5 leading-loose tracking-wide list-disc md:text-lg'>
-            <li className="my-2.5">Energía, ambiente y cambio climático </li>
-            <li className="my-2.5">Relaciones economía, ambiente y sociedad </li>
-            <li className="my-2.5">Transición energética </li>
-          </ul>
-
-        </article>
-
+        {sections}
       </main>
-
-    </div>
+    </Content>
   )
 }
