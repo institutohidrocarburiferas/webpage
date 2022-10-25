08354ad034635ac1ccdc4d5fadbc112c4980f95e
andresguanov <andresguanov@gmail.com>
2022-10-24 19:02:06 -0500

    Revert "[FIX] link component to anchor tag for use of RAM"
    This reverts commit 179d89c4eb0992e355a7b3bb0e7e4718115b846e.

--- components/Navbar.js
+++ components/Navbar.js
@@ -19,7 +19,7 @@ export function Navbar () {
     <div
       className="container px-2.5 flex flex-wrap items-center  justify-between w-full mx-auto  z-10 lg:flex-nowrap">
       {/* Logo */}
-      <Link href="/">
+      <Link href="/" >
         <a
           className='w-4/5 sm:w-auto'
           onClick={() => setCollapse('hidden')}
@@ -71,12 +71,14 @@ export function Navbar () {
 
                 {
                   (subItems === undefined)
-                    ? <a href={url}
+                    ? <Link href={url} >
+                      <a
                         className={`${pathname === url && `${styleCurrentPage} md:border-amber-600`} rounded transition-colors delay-75 md:border-b-2 md:rounded-none border-transparent hover:md:border-amber-700 block py-2 pl-3 pr-4 font-bold hover:bg-gray-100 md:hover:bg-transparent md:hover:text-amber-700  md:py-2 md:px-0 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-amber-700 dark:hover:md:border-amber-700 md:dark:hover:bg-transparent`}
                         onClick={() => setCollapse('hidden')}
                       >
                         {name}
                       </a>
+                    </Link>
                     : <div
                       className={`${subItems.some(({ url }) => url === pathname) && 'md:border-amber-600 md:border-b-2'} block py-2 pl-3 pr-4 text-gray-700 border rounded  md:hover:bg-transparent md:rounded-none md:border-0 md:hover:text-amber-700 dark:md:hover:text-gray-200 md:px-0 md:py-2 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-gray-200 md:dark:hover:bg-transparent dark:border-gray-600`}
                     >
@@ -93,12 +95,14 @@ export function Navbar () {
                               key={name}
                               className="py-1 text-sm font-semibold text-gray-700 md:bg-white dark:text-gray-200 dark:md:bg-gray-700"
                             >
-                                <a href={url}
+                              <Link href={url}>
+                                <a
                                   className={`${pathname === url && styleCurrentPage} block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white`}
                                   onClick={() => setCollapse('hidden')}
                                 >
                                   {name}
                                 </a>
+                              </Link>
                             </li>
                           ))
                         }
