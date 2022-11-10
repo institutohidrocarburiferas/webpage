206b339fd74bb744be60712c120cd41145cc3858
andresguanov <andresguanov@gmail.com>
2022-11-10 16:15:32 -0500

    [FIX] register form

--- components/RegisterButton.js
+++ components/RegisterButton.js
@@ -1,9 +1,11 @@
+// https://forms.gle/c55zGtRRenVvKpGKA
+
 export function RegisterButton () {
   return <a
-  className='flex text-black justify-center items-center w-auto px-6 py-2 text-sm sm:text-lg md:text-2xl font-semibold rounded hover:scale-105 bg-gradient-to-tr hover:from-amber-400 scroll-smooth hover:to-white from-amber-500 to-white'
-  href="https://forms.gle/c55zGtRRenVvKpGKA" target="_blank"
+  className='flex text-black justify-center items-center w-1/2 px-2 py-2 text-xs sm:text-lg md:text-2xl font-semibold rounded hover:scale-105 bg-gradient-to-tr hover:from-amber-400 scroll-smooth hover:to-white from-amber-500 to-white'
+  href="#"
   rel="noreferrer"
 >
-  Regístrate
+  Regístrate a partir del día viernes 18
 </a>
 }
