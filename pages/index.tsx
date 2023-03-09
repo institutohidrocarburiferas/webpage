ab06aa9d885f71afdf3833201bfada6031d814fa
institutohidrocarburiferas <instituto.hidrocarburiferas@gmail.com>
2023-03-09 16:30:50 -0500

    09-03-23

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,6 @@ const Home: NextPage<Props> = ({allPostsData}) => {
       </div>
     </main>
     <Footer image={pageData.image} />
-    <Visitors />
   </>
 }
 
@@ -134,4 +133,11 @@ export const getStaticProps: GetStaticProps = async ({locale}) => {
   }
 }
 
+export const Visitors: React.FC = () => {
+  return <a className="hidden" href="https://info.flagcounter.com/FUfR" rel="noreferrer" target="_blank">
+    <picture>
+      <img alt="Free counters!" src="https://s11.flagcounter.com/countxl/FUfR/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_1/pageviews_0/flags_0/percent_0/" />
+    </picture>
+  </a>
+}
 export default Home
