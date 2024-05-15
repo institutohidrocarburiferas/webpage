2571e52bf534a67337a7beec0ecd170c3e9f26e1
andresguanov <andresguanov@gmail.com>
2024-05-15 16:16:58 -0500

    Fix event button caption

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,7 @@ const Home: NextPage<Props> = ({allPostsData}) => {
                 <RegisterButton className='opacity-50 pointer-events-none' href={eventLinkRegister}>
                   Asiste al evento
                 </RegisterButton>
-                <span className="text-xs text-white lg:text-sm">Asiste al evento</span>
+                <span className="text-xs text-white lg:text-sm">Inscripciones a partir del 1 de Junio</span>
               </div>
             </div>
             <Countdown eventDate={eventDay} />
